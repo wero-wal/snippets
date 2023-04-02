bd62a36a7bab6a53d9f6d1bba46b243343cb19a4
W. Walczak <111140005+wero-wal@users.noreply.github.com>
2023-04-02 11:29:09 +0100

    fix: add filepath adjustor
    refactor: change brace formatting

--- SnippetCreator/File.cs
+++ SnippetCreator/File.cs
@@ -2,37 +2,33 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
-namespace SnippetCreator
-{
-	internal static class File
-	{
-		public const string Defaults = "defaults.txt";
-		public const string Forbidden = "forbidden-values.txt";
-		public const string Languages = "languages.txt";
+namespace SnippetCreator;
+public static class File {
+	
+	public const string FilePathAdjustor = "../../../"
+	public const string Defaults = FilePathAdjustor + "defaults.txt";
+	public const string Forbidden = FilePathAdjustor + "forbidden-values.txt";
+	public const string Languages = FilePathAdjustor + "languages.txt";
+	
+	public static List<string> Load(string filename) {
 		
-		public static List<string> Load(string filename)
-		{
-			List<string> fileContents = new List<string>{};
-			using(StreamReader sr = new StreamReader(filename))
-			{
-				while(!sr.EndOfStream)
-				{
-					fileContents.Add(sr.ReadLine());
-				}
-				sr.Close();
+		List<string> fileContents = new List<string>{};
+		
+		using(StreamReader sr = new StreamReader(filename)) {
+			while(!sr.EndOfStream) {
+				fileContents.Add(sr.ReadLine());
 			}
-			return fileContents;
+			sr.Close();
 		}
-		public static void Save(List<string> newFileContents, string filename, bool overwrite)
-		{
-			using(StreamWriter sw = new StreamWriter(filename, !overwrite))
-			{
-				foreach (string line in newFileContents)
-				{
-					sw.WriteLine(line);
-				}
-				sw.Close();
+		return fileContents;
+	}
+	public static void Save(List<string> newFileContents, string filename, bool overwrite) {
+		
+		using(StreamWriter sw = new StreamWriter(filename, !overwrite)) {
+			foreach (string line in newFileContents) {
+				sw.WriteLine(line);
 			}
+			sw.Close();
 		}
 	}
 }
